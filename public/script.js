56248da350b8ff7d7bcec4395fc74ed6d7917b3c
Patrick Küsters <kuesters.patrick@gmail.com>
2023-09-14 20:14:39 +0200

    polaroids added

--- public/script.js
+++ public/script.js
@@ -1,11 +1,24 @@
 const nextButton = document.getElementById('nextButton');
-const catImage = document.getElementById('catImage');
+const mainElement = document.querySelector('main'); // Targeting the main element
 
 nextButton.addEventListener('click', () => {
   fetch('/next')
     .then(response => response.json())
     .then(data => {
-      catImage.src = data.image;
+      const newItem = document.createElement('div');
+      newItem.className = 'item';
+      newItem.style.transform = `rotate(${Math.random() * 20 - 10}deg)`;
+
+      const newPolaroid = document.createElement('div');
+      newPolaroid.className = 'polaroid';
+
+      const newImage = document.createElement('img');
+      newImage.src = data.image;
+      newImage.alt = 'Cat Image';
+
+      newPolaroid.appendChild(newImage);
+      newItem.appendChild(newPolaroid);
+      mainElement.appendChild(newItem);
     })
     .catch(error => {
       console.error('Error fetching next cat image:', error.message);
